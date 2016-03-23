bbe4c6ceb71ca1735c727f7444a35f5a0d5100e5
yzerlaut <yann.zerlaut@gmail.com>
2016-03-23 22:32:35 +0100

    changing part names

--- pdf_output/compil.sh
+++ pdf_output/compil.sh
@@ -1,2 +1,2 @@
 cp ../tex/titlepage/page-de-garde1.pdf pg.pdf
-pdftk A=thesis.pdf B=pg.pdf cat A1 B1 A3-end output final_thesis.pdf
+pdftk A=thesis.pdf B=../tex/titlepage/page-de-garde1.pdf C=../tex/titlepage/ak.pdf cat A1 B1 C1-end A3-end output final_thesis.pdf
